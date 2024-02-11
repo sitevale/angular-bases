315ae0a15b699415eff824ec3332f7feefe5a91c
MaikelNais <sitevale@hotmail.com>
2024-02-11 16:37:07 +0100

    Clase 68

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
 
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunk',
+    power: 10
+  }]
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },{
+    name: 'Goku',
+    power: 9500
+  },{
+    name: 'Vegeta',
+    power: 7500
+  }];
 
+  public onNewCharacter(character: Character): void{
+    console.log('Desde MainPage', character);
+  }
 }
